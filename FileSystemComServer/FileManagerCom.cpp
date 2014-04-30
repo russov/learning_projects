a3442040d2dd31db0041bebe3c745111c904040a
Yura <y.rusovich@gmail.com>
2014-05-01 00:55:43 +0300

    added FileManagerCom

---
+++ FileSystemComServer/FileManagerCom.cpp
@@ -0,0 +1,242 @@
+﻿// FileManagerCom.cpp : Implementation of CFileManagerCom
+
+#include "stdafx.h"
+
+#include <vector>
+
+#include "FileManagerCom.h"
+
+bool CFileManagerCom::hasSubFolder(const CString &path)
+{
+	CFileFind FindFile; 
+	BOOL FlagFile = FindFile.FindFile(path + CString("\\*.*")); 
+
+	while (FlagFile) 
+	{ 
+		FlagFile = FindFile.FindNextFile();
+		if (FindFile.IsDirectory () && ! FindFile.IsDots () ) 
+			return true;
+	} 
+	return false;
+}
+
+bool CFileManagerCom::isFolder(const CString &path)
+{
+	if(FILE_ATTRIBUTE_DIRECTORY & GetFileAttributes(path))
+		return true;
+	return false;
+}
+
+STDMETHODIMP CFileManagerCom::getFolder(BSTR path, VARIANT * names)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	CFileFind FileFind;
+	std::vector <DataItem> vec;
+
+	BOOL FlagFile = FileFind.FindFile(CString(path + CString("\\*.*")));
+	while (FlagFile==1)
+	{
+		FlagFile = FileFind.FindNextFile();  
+		if((!FileFind.IsDots()))
+		{
+			CString hFileName = FileFind.GetFileName();
+			DataItem item;
+			SHFILEINFO Info;
+
+			item.name = hFileName.AllocSysString();
+			item.isFolder = FileFind.IsDirectory();
+			item.haveSubFolder = hasSubFolder(FileFind.GetFilePath());
+			
+			::SHGetFileInfo (FileFind.GetFilePath(), 0, &Info, sizeof (Info), SHGFI_ICON | SHGFI_SMALLICON); 
+			item.id = Info.iIcon;
+	
+			::SHGetFileInfo (FileFind.GetFilePath(),0,&Info,sizeof (Info),SHGFI_ICON | SHGFI_OPENICON | SHGFI_SMALLICON); 
+			item.idOpen = Info.iIcon;
+			
+			vec.push_back(item);
+		} 
+	}
+
+	IRecordInfo *pRI;
+	HRESULT hr;
+	
+	hr = GetRecordInfoFromGuids(LIBID_FileSystemComServerLib, 1, 0, 0x409, __uuidof(DataItem), &pRI);
+
+	SAFEARRAY *psa;
+	SAFEARRAYBOUND sab = {vec.size(), 0};
+	DataItem *pDataItem;
+
+	psa = SafeArrayCreateEx(VT_RECORD, 1, &sab, pRI);
+
+	pRI->Release();
+	pRI = NULL;
+	
+	hr = SafeArrayAccessData(psa, (void**)&pDataItem);
+	
+	for (size_t i = 0; i < vec.size(); ++i)
+		pDataItem[i] = vec[i];
+	hr = SafeArrayUnaccessData(psa);
+
+	names->vt = VT_ARRAY | VT_BYREF;
+	names->parray = psa;
+
+	return S_OK;
+}
+
+STDMETHODIMP CFileManagerCom::getRoot(VARIANT * names)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+	
+	DWORD dwSize = GetLogicalDriveStrings(0, NULL);
+	
+	CString disk;
+	
+	char* buff = new char[dwSize];
+
+	std::vector <DataItem> vec;
+
+	::GetLogicalDriveStringsA(dwSize, buff);
+	for (char* p = buff; p != dwSize + buff; ++p)
+	{
+		if (*p != '\0')
+		{
+			disk.AppendChar(*p);
+		}
+		else
+		{
+			if (!disk.IsEmpty())
+			{
+				DataItem item;
+				item.name = disk.AllocSysString();
+				item.isFolder = true;
+				item.haveSubFolder = true;
+
+				SHFILEINFO Info;
+
+				::SHGetFileInfo (disk, 0, &Info, sizeof (Info), SHGFI_ICON | SHGFI_SMALLICON); 
+				item.id = Info.iIcon;
+	
+				::SHGetFileInfo (disk, 0, &Info, sizeof (Info),SHGFI_ICON | SHGFI_OPENICON | SHGFI_SMALLICON); 
+				item.idOpen = Info.iIcon;
+
+				vec.push_back(item);
+			}
+			disk = "";
+		}
+	}
+	delete [] buff;
+	
+	IRecordInfo *pRI;
+	HRESULT hr;
+	
+	hr = GetRecordInfoFromGuids(LIBID_FileSystemComServerLib, 1, 0, 0x409, __uuidof(DataItem), &pRI);
+
+	SAFEARRAY *psa;
+	SAFEARRAYBOUND sab = {vec.size(), 0};
+	DataItem *pDataItem;
+
+	psa = SafeArrayCreateEx(VT_RECORD, 1, &sab, pRI);
+
+	pRI->Release();
+	pRI = NULL;
+	
+	hr = SafeArrayAccessData(psa, (void**)&pDataItem);
+	
+	for (size_t i = 0; i < vec.size(); ++i)
+		pDataItem[i] = vec[i];
+	hr = SafeArrayUnaccessData(psa);
+
+	names->vt = VT_ARRAY | VT_BYREF;
+	names->parray = psa;
+	
+	return S_OK;
+}
+
+STDMETHODIMP CFileManagerCom::getListIcon(VARIANT * icons)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+	
+	SHFILEINFO info; 
+
+	HIMAGELIST hlmg = (HIMAGELIST)::SHGetFileInfo(_T("c:\\"),0, &info, sizeof (info), SHGFI_SYSICONINDEX | SHGFI_SMALLICON); 
+	
+	icons->vt = VT_VOID | VT_BYREF;
+	icons->byref = hlmg;
+
+	return S_OK;
+}
+
+STDMETHODIMP CFileManagerCom::OpenItem(BSTR path, BOOL * isFolder)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	*isFolder = CFileManagerCom::isFolder(path);
+
+	if (*isFolder)
+		return S_OK;
+
+	::ShellExecute(NULL, _T("open"), CString(path), 0, 0, SW_SHOW);
+
+	return S_OK;
+}
+
+
+STDMETHODIMP CFileManagerCom::DeleteItem(BSTR path)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	std::string wszBuffer;
+	
+	TCHAR bufIn[_MAX_PATH + 1]; 
+	_tcscpy(bufIn, CString(path));   
+    bufIn[_tcslen(bufIn)+1]=0;    
+ 
+	TCHAR bufOut[_MAX_PATH + 1]; 
+	_tcscpy(bufOut, CString(path));   
+    bufOut[_tcslen(bufOut)+1]=0;  
+
+	SHFILEOPSTRUCT lpFileOp;
+	lpFileOp.hwnd=NULL;
+	lpFileOp.wFunc=FO_DELETE;
+	lpFileOp.pFrom=bufOut;
+	lpFileOp.pTo=bufIn;
+    lpFileOp.fFlags=FOF_NOCONFIRMATION;
+    lpFileOp.hNameMappings=NULL;
+    lpFileOp.lpszProgressTitle=NULL;
+    
+	SHFileOperation(&lpFileOp);
+
+	return S_OK;
+}
+
+
+STDMETHODIMP CFileManagerCom::CopyItem(const BSTR newPath, const BSTR oldPath)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	std::string wszBuffer;
+	
+	TCHAR bufIn[_MAX_PATH + 1]; 
+	_tcscpy(bufIn, CString(newPath));   
+    bufIn[_tcslen(bufIn)+1]=0;    
+ 
+	TCHAR bufOut[_MAX_PATH + 1]; 
+	_tcscpy(bufOut, CString(oldPath));   
+    bufOut[_tcslen(bufOut)+1]=0;  
+
+	SHFILEOPSTRUCT lpFileOp;
+	lpFileOp.hwnd=NULL;
+	lpFileOp.wFunc=FO_COPY;
+	lpFileOp.pFrom= bufOut;//oldPath;//L"d:\\temp\\new3.txt";    
+		//;
+	lpFileOp.pTo=bufIn;//newPath; //L"d:\\temp\\tt";
+		//newPath;
+    lpFileOp.fFlags=FOF_NOCONFIRMATION;
+    lpFileOp.hNameMappings=NULL;
+    lpFileOp.lpszProgressTitle=NULL;
+    
+	SHFileOperation(&lpFileOp);
+	
+	return S_OK;
+}
---
+++ FileSystemComServer/FileManagerCom.h
@@ -0,0 +1,65 @@
+// FileManagerCom.h : Declaration of the CFileManagerCom
+
+#pragma once
+#include "resource.h"       // main symbols
+
+
+
+#include "FileSystemComServer_i.h"
+
+
+
+#if defined(_WIN32_WCE) && !defined(_CE_DCOM) && !defined(_CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA)
+#error "Single-threaded COM objects are not properly supported on Windows CE platform, such as the Windows Mobile platforms that do not include full DCOM support. Define _CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA to force ATL to support creating single-thread COM object's and allow use of it's single-threaded COM object implementations. The threading model in your rgs file was set to 'Free' as that is the only threading model supported in non DCOM Windows CE platforms."
+#endif
+
+using namespace ATL;
+
+
+// CFileManagerCom
+
+class ATL_NO_VTABLE CFileManagerCom :
+	public CComObjectRootEx<CComSingleThreadModel>,
+	public CComCoClass<CFileManagerCom, &CLSID_FileManagerCom>,
+	public IDispatchImpl<IFileManagerCom, &IID_IFileManagerCom, &LIBID_FileSystemComServerLib, /*wMajor =*/ 1, /*wMinor =*/ 0>
+{
+public:
+	CFileManagerCom()
+	{
+	}
+
+DECLARE_REGISTRY_RESOURCEID(IDR_FILEMANAGERCOM)
+
+
+BEGIN_COM_MAP(CFileManagerCom)
+	COM_INTERFACE_ENTRY(IFileManagerCom)
+	COM_INTERFACE_ENTRY(IDispatch)
+END_COM_MAP()
+
+
+
+	DECLARE_PROTECT_FINAL_CONSTRUCT()
+
+	HRESULT FinalConstruct()
+	{
+		return S_OK;
+	}
+
+	void FinalRelease()
+	{
+	}
+
+protected:
+	bool hasSubFolder(const CString &path);
+	bool isFolder(const CString &path);
+	
+public:
+	STDMETHOD(getFolder)(BSTR path, VARIANT * names);
+	STDMETHOD(getRoot)(VARIANT * names);
+	STDMETHOD(getListIcon)(VARIANT * icons);
+	STDMETHOD(OpenItem)(BSTR path, BOOL * isFolder);
+	STDMETHOD(DeleteItem)(BSTR path);
+	STDMETHOD(CopyItem)(const BSTR newPath, const BSTR oldPath);
+};
+
+OBJECT_ENTRY_AUTO(__uuidof(FileManagerCom), CFileManagerCom)
