dd6f922c56c83096fb56039fb60f05c1dc8e06f4
russov <y.rusovich@gmail.com>
2014-05-23 18:26:52 +0300

    added manifest

--- FileSystemComServer/FileSystemComServer_i.h
+++ FileSystemComServer/FileSystemComServer_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed May 14 14:01:07 2014
+/* at Fri May 23 18:20:25 2014
  */
 /* Compiler settings for FileSystemComServer.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
